78cb2cf8602ac95a0055e3d6861c3cc3ae25da2c
OlehTysovskyi <oleg.tisovsky228@gmail.com>
2026-06-25 18:28:53 +0300

    fix(auth): export guard module dependencies

--- apps/api/src/admin/admin.module.ts
+++ apps/api/src/admin/admin.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { DesignsModule } from '../designs/designs.module';
 import { RequestsModule } from '../requests/requests.module';
+import { UsersModule } from '../users/users.module';
 import { AdminDesignsController } from './admin-designs.controller';
 import { AdminRequestsController } from './admin-requests.controller';
 
 @Module({
-  imports: [AuthModule, DesignsModule, RequestsModule],
+  imports: [AuthModule, DesignsModule, RequestsModule, UsersModule],
   controllers: [AdminDesignsController, AdminRequestsController],
 })
 export class AdminModule {}
