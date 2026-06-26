d354c01c4f0719127e188d5b4c888dd17b47cc32
OlehTysovskyi <oleg.tisovsky228@gmail.com>
2026-06-26 11:23:24 +0300

    feat(files): protect private file access (LW-048)

--- apps/api/src/files/files.module.ts
+++ apps/api/src/files/files.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from '../auth/auth.module';
 import { FileUploadValidationService } from './file-upload-validation.service';
+import { FilesController } from './files.controller';
 import { FilesService } from './files.service';
 import { FileAsset, FileAssetSchema } from './schemas/file-asset.schema';
 import { S3StorageService } from './s3-storage.service';
 
 @Module({
   imports: [
+    AuthModule,
     ConfigModule,
     MongooseModule.forFeature([
       {
@@ -16,6 +19,7 @@ import { S3StorageService } from './s3-storage.service';
       },
     ]),
   ],
+  controllers: [FilesController],
   providers: [FilesService, FileUploadValidationService, S3StorageService],
   exports: [FilesService, FileUploadValidationService, S3StorageService, MongooseModule],
 })
