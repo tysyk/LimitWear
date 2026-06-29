d4024ebc86b0fe8a509931e480d712a2743fc72f
OlehTysovskyi <oleg.tisovsky228@gmail.com>
2026-06-29 14:14:08 +0300

    feat(delivery): add create ttn endpoint (LW-078)

--- apps/api/src/admin/admin.module.ts
+++ apps/api/src/admin/admin.module.ts
@@ -1,16 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { DeliveryModule } from '../delivery/delivery.module';
 import { DropsModule } from '../drops/drops.module';
 import { DesignsModule } from '../designs/designs.module';
 import { PaymentsModule } from '../payments/payments.module';
 import { RequestsModule } from '../requests/requests.module';
 import { UsersModule } from '../users/users.module';
+import { AdminDeliveryController } from './admin-delivery.controller';
 import { AdminDesignsController } from './admin-designs.controller';
 import { AdminDropsController } from './admin-drops.controller';
 import { AdminRequestsController } from './admin-requests.controller';
 
 @Module({
-  imports: [AuthModule, DesignsModule, DropsModule, PaymentsModule, RequestsModule, UsersModule],
-  controllers: [AdminDesignsController, AdminDropsController, AdminRequestsController],
+  imports: [
+    AuthModule,
+    DeliveryModule,
+    DesignsModule,
+    DropsModule,
+    PaymentsModule,
+    RequestsModule,
+    UsersModule,
+  ],
+  controllers: [
+    AdminDeliveryController,
+    AdminDesignsController,
+    AdminDropsController,
+    AdminRequestsController,
+  ],
 })
 export class AdminModule {}
